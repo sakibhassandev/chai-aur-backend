e02bf9758d978794775f2df030cb1a1a4d01d0fa
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-11-20 15:25:42 +0600

    add multer file system setup

---
+++ src/middlewares/multer-middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import path from "path";
+import crypto from "crypto";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, function (err, bytes) {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+
+export const upload = multer({ storage });
